958e67df5cafeaa84c58343b0de83d9604622d9a
Tomi Parviainen <tomi.parviainen@gmail.com>
2022-06-12 09:52:15 +0300

    Add source generator packages

---
+++ src/QueryStringGenerator/SourceGenerator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+
+namespace QueryStringGenerator
+{
+    [Generator]
+    internal class SourceGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            context.RegisterForSyntaxNotifications(() => new SyntaxReceiver());
+        }
+    }
+}
---
+++ src/QueryStringGenerator/SyntaxReceiver.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+
+namespace QueryStringGenerator
+{
+    internal class SyntaxReceiver : ISyntaxContextReceiver
+    {
+        public void OnVisitSyntaxNode(GeneratorSyntaxContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
