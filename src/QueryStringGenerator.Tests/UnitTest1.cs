f42877193ca809ce092c09bb42159f80f8dd2cac
Tomi Parviainen <tomi.parviainen@gmail.com>
2022-06-14 19:44:47 +0300

    Add meaningul unit tests

--- src/QueryStringGenerator.Tests/UnitTest1.cs
+++ src/QueryStringGenerator.Tests/UnitTest1.cs
@@ -1,10 +1,37 @@
+using System.Net;
+
 namespace QueryStringGenerator.Tests;
+
+[QueryString]
+public partial class Person
+{
+    public string? FirstName { get; set; }
+    public string LastName { get; set; } = default!;
+    public int? Age { get; set; }
+}
+
+public class Student : Person
+{
+    public string? Institute { get; set; }
+}
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData("John", "Doe")]
+    public void ReferenceTypeValuesEncodedCorrectly(string firstName, string lastName)
     {
+        // Arrange
+        var person = new Person
+        {
+            FirstName = firstName,
+            LastName = lastName
+        };
 
+        // Act
+        var queryString = person.ToQueryString();
+
+        // Assert
+        Assert.Equal($"&firstname={WebUtility.UrlEncode(firstName)}&lastname={WebUtility.UrlEncode(lastName)}", queryString);
     }
 }
