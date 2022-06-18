b1d20f291ec9367c67509a7b7ca1fe02620e66e8
Tomi Parviainen <tomi.parviainen@gmail.com>
2022-06-18 14:56:02 +0300

    Add extension method to get class modifiers
    All class modifiers cannot be used because the generated extension class cannot be 'partial' in C# thus this extension method returns all except partial keyword to caller in a string.

---
+++ src/QueryStringGenerator/Extensions/SyntaxExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Linq;
+
+namespace QueryStringGenerator.Extensions
+{
+    internal static class SyntaxExtensions
+    {
+        /// <summary>
+        /// Returns the class modifiers. Partial keyword is excluded due the fact that extension
+        /// methods cannot be declared inside 'partial static class'.
+        /// </summary>
+        public static string GetModifiers(this ClassDeclarationSyntax cds) =>
+            string.Join(" ", cds.Modifiers.Where(m => !m.IsKind(SyntaxKind.PartialKeyword)));
+    }
+}
