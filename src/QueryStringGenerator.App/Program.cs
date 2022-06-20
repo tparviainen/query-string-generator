73926d4e77064d9cab3a19ac8de48e17b58620d5
Tomi Parviainen <tomi.parviainen@gmail.com>
2022-06-20 19:18:31 +0300

    Add example code described in README.md

--- src/QueryStringGenerator.App/Program.cs
+++ src/QueryStringGenerator.App/Program.cs
@@ -20,3 +20,14 @@ var childClass = new ChildClass
 Console.WriteLine("Generated query strings:");
 Console.WriteLine("- base: " + baseClass.ToQueryString());
 Console.WriteLine("- child: " + childClass.ToQueryString() + childClass.ChildQueryString());
+
+/*
+ * README.md example model and the output
+ */
+var model = new Model
+{
+    Limit = 10,
+    Sort = "Price"
+};
+
+Console.WriteLine($"Query string: {model.ToQueryString()}");
