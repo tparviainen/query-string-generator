645767a364ad9596de258aef5a22503e2b538497
Tomi Parviainen <tomi.parviainen@gmail.com>
2023-07-18 10:22:09 +0300

    Add unit tests for attribute

---
+++ src/QueryStringGenerator.Tests/AttributeTests.cs
@@ -0,0 +1,58 @@
+namespace QueryStringGenerator.Tests;
+
+[UsesVerify]
+public class AttributeTests
+{
+    [Fact]
+    public Task ClassWithoutQueryStringAttributeDoesNotGenerateExtensionMethod()
+    {
+        // The source code to test
+        var source = """
+using System;
+
+namespace QueryStringGenerator.Tests.Models;
+
+[Serializable]
+public class Person
+{
+}
+""";
+
+        // Pass the source code to our helper and snapshot test the output
+        return TestHelper.Verify(source);
+    }
+
+    [Fact]
+    public Task QueryStringWithoutAttributeSuffixIsFoundFromClass()
+    {
+        // The source code to test
+        var source = """
+namespace QueryStringGenerator.Tests.Models;
+
+[QueryString]
+public class Person
+{
+}
+""";
+
+        // Pass the source code to our helper and snapshot test the output
+        return TestHelper.Verify(source);
+    }
+
+    [Fact]
+    public Task QueryStringWithAttributeSuffixIsFoundFromClass()
+    {
+        // The source code to test
+        var source = """
+namespace QueryStringGenerator.Tests.Models;
+
+[QueryStringAttribute]
+public class Person
+{
+}
+""";
+
+        // Pass the source code to our helper and snapshot test the output
+        return TestHelper.Verify(source);
+    }
+}
